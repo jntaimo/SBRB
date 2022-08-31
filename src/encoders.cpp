46e33bf50cdc248090f9cf2a57a07684fe89e102
jntaimo <jntaimo@gmail.com>
2022-08-31 12:59:59 -0400

    Added right encoder support

--- src/encoders.cpp
+++ src/encoders.cpp
@@ -2,65 +2,78 @@
 #include <Encoder.h>
 #include "encoders.h"
 
-#define DIR1 15
-#define PWM1 2
-#define DIR2 4
-#define PWM2 5
+//Encoder pulses per revolution for 312 RPM Bilda motor
 #define PPR 751.8
+//Pinouts for wiring
+#define encLPinA 20
+#define encLPinB 21
+#define encRPinA 22
+#define encRPinB 23
 
-
-//Encoder Variables
+//Left Encoder Variables
 long currentLPos = 0;
 long prevLPos = 0;
 long currentLTime = 0;
 long prevLTime = 0;
 float radL = 0;
 
+//Right Encoder Variables
+long currentRPos = 0;
+long prevRPos = 0;
+long currentRTime = 0;
+long prevRTime = 0;
+float radR = 0;
+
+//Smoothing constant for filtering
+//alpha closer to 0 is closer to raw data, very fast
+//alpha closer to 1 is more smooth, but more sluggish
 float alpha = 0.15;
 float filtRadL = 0;
-Encoder encL(40, 41);
+float filtRadR = 0;
+
+Encoder encL(encLPinA, encLPinB);
+Encoder encR(encRPinA, encRPinB);
 
 long prevTime = 0;
 int delayTime = 25;
 
-void readEncoders();
+
 void encoderSetup() {
-  //Configure motor pins as outputs
-  pinMode(DIR1, OUTPUT);
-  pinMode(PWM1, OUTPUT);
-  pinMode(DIR2, OUTPUT);
-  pinMode(PWM2, OUTPUT);
-  
   Serial.begin(115200);
 }
 
 
 
 void encoderLoop() {
-  //GO forward
-  digitalWrite(DIR1, HIGH);
-  digitalWrite(DIR2, LOW);
-  analogWrite(PWM1, 255);
-  analogWrite(PWM2, 0);
-
   readEncoders();
   filtRadL = alpha*radL + (1-alpha)*filtRadL;
-
+  filtRadR = alpha*radR + (1-alpha)*filtRadR;
   //Only print data if the current time is more than 25 ms greater than the last time it was printed
-  if(millis()>prevTime+delayTime){
+  if(millis() > prevTime+delayTime){
     prevTime = millis();
     Serial.print(radL);
     Serial.print("\t");
-    Serial.println(filtRadL);
+    Serial.print(filtRadL);
+    Serial.print("\t");
+    Serial.print(radR);
+    Serial.print(filtRadR);
   }
 
   delay(2);
 }
 
 void readEncoders(){
+  //update left encoder
   currentLPos = encL.read();
   currentLTime = micros();
   radL = float(currentLPos-prevLPos)*2*PI/(PPR*(currentLTime - prevLTime))*1e6;
   prevLPos = currentLPos; 
   prevLTime = currentLTime; 
+
+  //update right encoder
+  currentRPos = encR.read();
+  currentRTime = micros();
+  radR = float(currentRPos-prevRPos)*2*PI/(PPR*(currentRTime - prevRTime))*1e6;
+  prevRPos = currentRPos; 
+  prevRTime = currentRTime;   
 }
